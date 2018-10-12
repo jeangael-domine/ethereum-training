692e38a8673bf3187ac03ba5b0683f301bb0277e
m428823 <jgdomine@airfrance.fr>
2018-10-12 09:24:20 +0200

    Contract real deployment config

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+// Capital letter because we are calling the Constructor
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+// Mnemonic as first argument to retrieve the accounts and the URL to the Ethereum network
+const provider = new HDWalletProvider(
+    'educate design explain kidney climb lift elegant need chase embrace depend sibling',
+    'https://rinkeby.infura.io/v3/7697444efe2e4751bc2f20f7f4549c36'
+);
+
+// Low letter because it is an instance
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ['Hi there!'] })
+        .send({from: accounts[0], gas: '1000000'});
+
+    console.log('Contract deployed to ', result.options.address);
+};
+
+deploy();
