8b003416312ffeb29926416511f4054ba92465c3
m428823 <jgdomine@airfrance.fr>
2018-10-12 08:26:58 +0200

    Test of contract methods

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -23,4 +23,18 @@ describe('Inbox', () => {
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+
+        assert.equal(message, "Hi there!");
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage("bye").send({ from: accounts[0], gas: '1000000' });
+
+        const message = await inbox.methods.message().call();
+
+        assert.equal(message, "bye");
+    });
 });
