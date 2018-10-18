ada41916309528ca6d8df97f6219c6a063f98fe1
root <root@tlssbcld0044.france.airfrance.fr>
2018-10-18 08:46:40 +0000

    Requests list fix

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -76,17 +76,23 @@ class CampaignShow extends Component {
             <Layout>
                 <h3>Campaign details</h3>
                 <Grid>
-                    <Grid.Column width={10}>
-                        { this.renderCards() }
-                        <Link route={ `/campaigns/${this.props.campaignAddress }/requests` }>
-                            <a>
-                                <Button primary>View requests</Button>
-                            </a>
-                        </Link>
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <ContributeForm campaignAddress={ this.props.campaignAddress } />
-                    </Grid.Column>
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                                { this.renderCards() }
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            <ContributeForm campaignAddress={ this.props.campaignAddress } />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Link route={ `/campaigns/${this.props.campaignAddress }/requests` }>
+                                <a>
+                                    <Button primary>View requests</Button>
+                                </a>
+                            </Link>
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </Layout>
         )
