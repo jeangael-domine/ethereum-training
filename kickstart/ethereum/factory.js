3d48204379a06060c0110cc0e8aa491c4a33d43a
m428823 <jgdomine@airfrance.fr>
2018-10-18 08:50:35 +0200

    Getting the campaign summary

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -1,6 +1,7 @@
 import web3 from './web3';
 import compiledFactory from './build/CampaignFactory.json';
 
+// The second argument is the address of the deployed CampaignFactory contract
 const instance = new web3.eth.Contract(
     JSON.parse(compiledFactory.interface),
     '0x241C480432D50FE12104f0F8E2B27F6726765AE4'
--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import web3 from '../../ethereum/web3';
+import compiledCampaign from '../../ethereum/build/Campaign.json';
 
 class CampaignShow extends Component {
+
+    static async getInitialProps(props) {
+        const campaign = new web3.eth.Contract(
+            JSON.parse(compiledCampaign.interface),
+            props.query.address
+        );
+
+        const campaignDetails = await campaign.methods.getSummary().call();
+
+        return { campaignDetails };
+    }
+
     render() {
         return (
             <Layout>
-                <h3>Campaign details</h3>
+                <h3>Campaign details {this.props.minimumContribution}</h3>
             </Layout>
         )
     }
